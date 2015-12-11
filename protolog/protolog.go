e571e5959ec552ebe032cca671ab0d48562e95e5
Peter Edge <peter.edge@gmail.com>
2015-12-11 12:14:19 +0100

    updates for protolog

--- protolog/protolog.go
+++ protolog/protolog.go
@@ -8,7 +8,7 @@ import (
 )
 
 func init() {
-	dlog.SetLogger(NewLogger(protolog.NewStandardLogger(protolog.NewStandardWritePusher(protolog.NewFileFlusher(os.Stderr)))))
+	dlog.SetLogger(NewLogger(protolog.NewLogger(protolog.NewDefaultTextWritePusher(protolog.NewFileFlusher(os.Stderr)), protolog.LoggerOptions{})))
 }
 
 type logger struct {
